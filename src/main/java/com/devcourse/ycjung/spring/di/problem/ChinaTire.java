e59d57414ec2adcfc3f6ea2c432c671ef42db106
Yechan Jung <skfk286@hanmail.net>
2024-08-08 16:27:49 +0900

    DI 관점에 대한 공부.. 그중에 소스코드에서 문제점 발생되는 부분 체크 의존성에 관한. KoreaTire 와 ChinaTire 의 변경에 따른 의존성 문제 고찰

---
+++ src/main/java/com/devcourse/ycjung/spring/di/problem/ChinaTire.java
@@ -0,0 +1,10 @@
+package com.devcourse.ycjung.spring.di.problem;
+
+// oracle 버전으로 작성된 Repository 클래스, DB 실행을 담당
+public class ChinaTire {
+    private String model;
+
+    public String getModel() {
+        return model;
+    }
+}
