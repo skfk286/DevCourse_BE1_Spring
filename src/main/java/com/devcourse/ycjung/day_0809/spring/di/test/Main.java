790b0f96005e780c3b0ed2f15f72cfe1305b1a8a
Yechan Jung <skfk286@hanmail.net>
2024-08-09 09:46:12 +0900

    파일 구조 변경에 따른 패키지 수정

---
+++ src/main/java/com/devcourse/ycjung/day_0809/spring/di/test/Main.java
@@ -0,0 +1,17 @@
+package com.devcourse.ycjung.day_0809.spring.di.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class Main {
+    public static final String FILE_SYSTEM_XML_PATH = "/src/main/java/com/devcourse/ycjung/spring/di/test/beans.xml";
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new FileSystemXmlApplicationContext(FILE_SYSTEM_XML_PATH);
+
+        Foo foo = (Foo)context.getBean("foo");
+        foo.getBar().print();
+
+    }
+}
